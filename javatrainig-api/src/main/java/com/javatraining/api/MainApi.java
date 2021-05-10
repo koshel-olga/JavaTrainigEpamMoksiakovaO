416ff4c9d556d7465cb3ef8d1b01d57fd11639e0
Olga Moksiakova <koshel.olga86@gmail.com>
2021-05-10 16:30:01 +0400

    lesson12

--- javatrainig-api/src/main/java/com/javatraining/api/MainApi.java
+++
@@ -1,9 +0,0 @@
-package com.javatraining.api;
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
-public class MainApi {
-    public static void main(String[] args) {
-        log.info("Hello World");
-    }
-}
